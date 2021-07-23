2b0614fc78bf0c00a553c41ec3cd11b6d8883da3
Arthur <37938234+supremestranger@users.noreply.github.com>
2021-07-23 18:01:08 +0300

    Update EcsSystemsExtensions.cs

--- Runtime/Helpers/EcsSystemsExtensions.cs
+++ Runtime/Helpers/EcsSystemsExtensions.cs
@@ -1,25 +1,25 @@
-﻿using Leopotam.EcsLite;
+using Leopotam.EcsLite;
 using Leopotam.EcsLite.ExtendedSystems;
 
 namespace LeoEcsPhysics
 {
     public static class EcsSystemsExtensions
     {
-        public static EcsSystems DelHerePhysics(this EcsSystems ecsSystems)
+        public static EcsSystems DelHerePhysics(this EcsSystems ecsSystems, string worldName = null)
         {
-            ecsSystems.DelHere<OnTriggerEnterEvent>();
-            ecsSystems.DelHere<OnTriggerStayEvent>();
-            ecsSystems.DelHere<OnTriggerExitEvent>();
-            ecsSystems.DelHere<OnCollisionEnterEvent>();
-            ecsSystems.DelHere<OnCollisionStayEvent>();
-            ecsSystems.DelHere<OnCollisionExitEvent>();
-            ecsSystems.DelHere<OnControllerColliderHitEvent>();
-            ecsSystems.DelHere<OnTriggerEnter2DEvent>();
-            ecsSystems.DelHere<OnTriggerStay2DEvent>();
-            ecsSystems.DelHere<OnTriggerExit2DEvent>();
-            ecsSystems.DelHere<OnCollisionEnter2DEvent>();
-            ecsSystems.DelHere<OnCollisionStay2DEvent>();
-            ecsSystems.DelHere<OnCollisionExit2DEvent>();
+            ecsSystems.DelHere<OnTriggerEnterEvent>(worldName);
+            ecsSystems.DelHere<OnTriggerStayEvent>(worldName);
+            ecsSystems.DelHere<OnTriggerExitEvent>(worldName);
+            ecsSystems.DelHere<OnCollisionEnterEvent>(worldName);
+            ecsSystems.DelHere<OnCollisionStayEvent>(worldName);
+            ecsSystems.DelHere<OnCollisionExitEvent>(worldName);
+            ecsSystems.DelHere<OnControllerColliderHitEvent>(worldName);
+            ecsSystems.DelHere<OnTriggerEnter2DEvent>(worldName);
+            ecsSystems.DelHere<OnTriggerStay2DEvent>(worldName);
+            ecsSystems.DelHere<OnTriggerExit2DEvent>(worldName);
+            ecsSystems.DelHere<OnCollisionEnter2DEvent>(worldName);
+            ecsSystems.DelHere<OnCollisionStay2DEvent>(worldName);
+            ecsSystems.DelHere<OnCollisionExit2DEvent>(worldName);
             
             return ecsSystems;
         }
